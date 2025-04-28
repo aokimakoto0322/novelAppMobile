8cd5a8dc43522f9f3d8abf8392e64f16e7cc13a8
AokiMakoto <aokimakoto0322@gmail.com>
2025-04-28 10:31:33 +0900

    画像表示エリアを背景からStackにして全画面表示するよう変更

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -53,52 +53,57 @@ class _GameScreenState extends State<GameScreen> {
         onTap: () {
           _showItem();
         },
-        child: AnimatedSwitcher(
-          duration: const Duration(seconds: 2),
-          transitionBuilder: (Widget child, Animation<double> animation) {
-            return FadeTransition(opacity: animation, child: child);
-          },
-          child: Container(
-            key: ValueKey(backgroundImage),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/$backgroundImage'),
-                fit: BoxFit.cover
-              )
-            ),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  // テキストエリア
-                  Container(
-                    color: Colors.brown.withAlpha(200),
-                    alignment: Alignment.topLeft,
-                    height: 150,
-                    child: Container(
-                      margin: EdgeInsets.only(left: 20, right: 20, top: 10),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: AnimatedTextKit(
-                          key: ValueKey<String>(widget.allStory[currentIndex].word),
-                          animatedTexts: [
-                            TyperAnimatedText(
-                              widget.allStory[currentIndex].word,
-                              textStyle: const TextStyle(
-                                fontSize: 18
-                              ),
-                            )
-                          ],
-                          totalRepeatCount: 1,
-                          displayFullTextOnTap: true
-                        )
-                      ),
-                    ),
-                  )
-                ],
+        behavior: HitTestBehavior.deferToChild,
+        child: Stack(
+          children: <Widget>[
+            // 画像表示エリア
+            AnimatedSwitcher(
+              duration: const Duration(seconds: 2),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return FadeTransition(opacity: animation, child: child);
+              },
+              child: Container(
+                key: ValueKey(backgroundImage),
+                child: Image.asset(
+                  'images/$backgroundImage',
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
+                  
+            // テキストエリア
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                color: Colors.brown.withAlpha(200),
+                alignment: Alignment.topLeft,
+                height: 150,
+                child: Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: AnimatedTextKit(
+                      key: ValueKey<String>(widget.allStory[currentIndex].word),
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          widget.allStory[currentIndex].word,
+                          textStyle: const TextStyle(
+                            fontSize: 18
+                          ),
+                        )
+                      ],
+                      totalRepeatCount: 1,
+                      displayFullTextOnTap: true
+                    )
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
