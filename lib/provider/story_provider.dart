e989122d95abe45cee1caad75f2bcb3e501d82fd
AokiMakoto <aokimakoto0322@gmail.com>
2026-02-20 14:03:00 +0900

    flutterのバージョン更新 それに伴うパッケージの更新

--- lib/provider/story_provider.dart
+++ lib/provider/story_provider.dart
@@ -1,28 +1,6 @@
-import 'package:flutter_nobel_app/provider/backlog_provider.dart';
-import 'package:flutter_nobel_app/provider/choice_provider.dart';
-import 'package:flutter_nobel_app/provider/database_provider.dart';
-import 'package:flutter_nobel_app/provider/common_story_api_provider.dart';
-import 'package:flutter_nobel_app/provider/story_repository_provider.dart';
 import 'package:flutter_nobel_app/state/story_state.dart';
 import 'package:flutter_nobel_app/usecase/story_usecase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:just_audio/just_audio.dart';
 
-final storyUsecaseProvider = StateNotifierProvider<StoryUsecase, StoryState>((ref) {
-  final db = ref.watch(databaseProvider);
-  final choiceRepository = ref.watch(choiceRepositoryProvider);
-  final storyRepository = ref.watch(storyRepositoryProvider);
-  final storyApi = ref.watch(commonStoryApiProvider);
-  final backlogUsecase = ref.watch(backlogUsecaseProvider);
-
-  final audioPlayer = AudioPlayer();
-
-  return StoryUsecase(
-    db: db,
-    choiceRepository: choiceRepository,
-    storyRepository: storyRepository,
-    commonStoryApi: storyApi,
-    backlogUsecase: backlogUsecase,
-    audioPlayer: audioPlayer
-  );
-});
+final storyUsecaseProvider =
+    NotifierProvider<StoryUsecase, StoryState>(StoryUsecase.new);
