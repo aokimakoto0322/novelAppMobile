337af69de611a5501f69fcca54c524157d6c857c
AokiMakoto <aokimakoto0322@gmail.com>
2026-03-06 22:58:15 +0900

    フォントの変更

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends ConsumerWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        textTheme: GoogleFonts.notoSansJpTextTheme(),
+        textTheme: GoogleFonts.kosugiMaruTextTheme(),
       ),
       routerConfig: router,
     );
