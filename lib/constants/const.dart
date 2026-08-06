db5a582a12963f5854c77483e8446bce6e90d34c
AokiMakoto <aokimakoto0322@gmail.com>
2026-08-06 20:57:07 +0900

    バックログ画面にキャラアイコンが表示されるよう修正

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -6,4 +6,10 @@ class Const {
     '場所選択画面': 2,
     '選択肢画面': 3
   };
+
+  static const Map<String, String> CHARACTER_IMAGE_MAP = {
+    '小金 秋': 'images/icons/mob_icon.png',
+    'ヒロインB': 'images/icons/mob_icon.png',
+    // 他のキャラクターもここに追加
+  };
 }
